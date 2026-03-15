9733ba5e70503515163ee4e2ca66a9f86e8787af
burni <mathieu.houbron@campus-centre.fr>
2026-03-15 14:06:47 +0100

    work in progress

---
+++ Database.php
@@ -0,0 +1,35 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $connexion;
+
+    // Paramètres de connexion
+    private $host = 'localhost';
+    private $db_name = 'MHOUBRON_';
+    private $username = 'root';
+    private $password = '';
+
+    private function __construct() {
+        try {
+            $this->connexion = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnexion() {
+        return $this->connexion;
+    }
+}
